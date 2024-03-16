c07e34ccc5137e2ec4e61ebfc4fb4b9e58fac1e9
Muhammad Ridho <muhammadridho.mail@gmail.com>
2024-03-16 14:30:02 +0700

    implement auth to userRouter

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,13 +1,21 @@
 const
     express = require('express'),
     userRouter = express.Router(),
+    passport = require('passport'),
+    Auth = require('../lib/auth.js'),
     { UserController } = require('../controller/userController.js');
 
 userRouter
-    .get('/', UserController.getUser)
     .post('/register', UserController.register)
+    .post('/admin/register', UserController.registerAdmin)
     .post('/login', UserController.login)
-    .delete('/:id', UserController.deleteUser)
-    .put('/:id', UserController.editUser)
+
+userRouter.use(passport.initialize(), Auth.authentication, Auth.authorize)
+
+userRouter
+    .get('/profile', UserController.getUser)
+    .put('/profile', UserController.editUser)
+    .delete('/', UserController.deleteUser)
+
 
 module.exports = userRouter
