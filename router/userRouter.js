739414d432bee9b339478a675c6579767e3cc87d
Muhammad Ridho <muhammadridho.mail@gmail.com>
2024-03-27 00:28:17 +0700

    add address route on userRouter

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,9 +1,11 @@
+
 const
     express = require('express'),
     userRouter = express.Router(),
     passport = require('passport'),
     Auth = require('../lib/auth.js'),
-    { UserController } = require('../controller/userController.js');
+    { UserController } = require('../controller/userController.js'),
+    { AddressController } = require('../controller/addressController.js');
 
 userRouter
     .post('/register', UserController.register)
@@ -11,12 +13,19 @@ userRouter
     .post('/login', UserController.login)
     .get('/verify/:id', UserController.verifyUser)
 
-userRouter.use(passport.initialize(), Auth.authentication, Auth.authorize)
+userRouter.use(passport.initialize(), Auth.authentication)
 
 userRouter
-    .get('/profile', UserController.getUser)
-    .put('/profile', UserController.editUser)
-    .delete('/', UserController.deleteUser)
+    .get('/profile', Auth.authorize , UserController.getUser)
+    .put('/profile', Auth.authorize , UserController.editUser)
+    .delete('/', Auth.authorize , UserController.deleteUser)
 
+userRouter
+    .get('/addresses', Auth.authorizeUser, AddressController.getAllAddresses)
+    .get('/addresses/:id', Auth.authorizeUser, AddressController.getUserAddressById)
+    .post('/addresses', Auth.authorizeUser, AddressController.createNewAddress)
+    .put('/addresses/:id', Auth.authorizeUser, AddressController.updateAddress)
+    .patch('/addresses/:id', Auth.authorizeUser, AddressController.updateMainAddress)
+    .delete('/addresses/:id', Auth.authorizeUser, AddressController.deleteAddress)
 
 module.exports = userRouter
