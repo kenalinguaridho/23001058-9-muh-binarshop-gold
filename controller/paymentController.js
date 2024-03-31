61a3ca2d5a81d9bea53c187be32efe3464add190
Muhammad Ridho <muhammadridho.mail@gmail.com>
2024-04-01 01:13:48 +0700

    add new endpoint to delete and update payment method

--- controller/paymentController.js
+++ controller/paymentController.js
@@ -2,31 +2,88 @@ const { PaymentMethod } = require('../models')
 const { responseJSON } = require('../helpers/response');
 
 class PaymentController {
-    static getPayment = async(req, res) => {
-        let statusCode = 200
-        let status
-        let message
-        let payments
-
+    static getAllPayment = async(req, res) => {
+        
         try {
 
-            payments = await PaymentMethod.findAll({})
+            const payments = await PaymentMethod.findAll({
+                attributes: ['id', 'name']
+            })
 
-            console.log(payments)
-            if (!payments) {
-                throw error
-            }
+            return res.status(200).json(responseJSON(payments))
 
         } catch (error) {
 
-            status = 'failed'
-            statusCode = 404
-            message = 'No payment found'
-            payments = null
+            return res.status(500).json(responseJSON(null, 'failed', 'failed while fetching payment method'))
 
         }
 
-        return res.status(statusCode).json(responseJSON(payments, status, message))
+    }
+
+    static createNewPayment = async (req, res) => {
+        try {
+            
+            const payload = {
+                name: req.body.name
+            }
+
+            const payment = await PaymentMethod.create(payload)
+
+            return res.status(201).json(responseJSON(payment))
+
+        } catch (error) {
+            
+            return res.status(500).json(responseJSON(null, 'failed', 'failed while creating new payment method'))
+
+        }
+    }
+
+    static updatePayment = async (req, res) => {
+        try {
+
+            const id = req.params.id
+
+            const payment = await PaymentMethod.findOne({
+                where: {
+                    id
+                }
+            })
+
+            if(!payment) {
+                return res.status(404).json(responseJSON(null, 'failed', `no payment method with id ${id}`))
+            }
+
+            await payment.update({
+                name: req.body.name
+            })
+
+            return res.status(200).json(responseJSON(payment))
+
+        } catch (error) {
+
+            return res.status(500).json(responseJSON(null, 'failed'))
+
+        }
+    }
+
+    static deletePayment = async (req, res) =>{
+        try {
+            
+            const id = req.params.id
+
+            const payment = await PaymentMethod.findByPk(id)
+
+            if (!payment) {
+                return res.status(404).json(responseJSON(null, 'failed', `no payment method withid ${id}`))
+            }
+
+            await payment.destroy()
+
+            return res.status(200).json(responseJSON(null))
+
+        } catch (error) {
+            return res.status(500).json(responseJSON(null, 'failed', 'failed while deleting payment method'))
+        }
     }
 }
 
--- router/paymentRouter.js
+++ router/paymentRouter.js
@@ -1,10 +1,18 @@
 const
     express = require('express'),
     paymentRouter = express.Router(),
-    { PaymentController } = require('../controller/paymentController');
+    { PaymentController } = require('../controller/paymentController'),
+    passport = require('passport'),
+    Auth = require('../lib/auth')
 
 paymentRouter
-    .get('/', PaymentController.getPayment)
-    
+    .get('/', PaymentController.getAllPayment)
+
+paymentRouter.use(passport.initialize(), Auth.authentication, Auth.authorizeAdmin)
+
+paymentRouter
+    .post('/', PaymentController.createNewPayment)
+    .put('/:id', PaymentController.updatePayment)
+    .delete('/:id', PaymentController.deletePayment)
 
 module.exports = paymentRouter 
