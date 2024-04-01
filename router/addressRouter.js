33f977e6906a3a31427960a36e850e88ecd5455a
Muhammad Ridho <muhammadridho.mail@gmail.com>
2024-04-02 00:41:21 +0700

    fixing api endpoint for address router

--- router/addressRouter.js
+++ router/addressRouter.js
@@ -8,11 +8,11 @@ const
 addressRouter.use(passport.initialize(), Auth.authentication, Auth.authorizeUser)
 
 addressRouter
-    .get('/addresses', AddressController.getAllAddresses)
-    .get('/addresses/:id', AddressController.getUserAddressById)
-    .post('/addresses', AddressController.createNewAddress)
-    .put('/addresses/:id', AddressController.updateAddress)
-    .patch('/addresses/:id', AddressController.updateMainAddress)
-    .delete('/addresses/:id', AddressController.deleteAddress)
+    .get('/', AddressController.getAllAddresses)
+    .get('/:id', AddressController.getUserAddressById)
+    .post('/', AddressController.createNewAddress)
+    .put('/:id', AddressController.updateAddress)
+    .patch('/:id', AddressController.updateMainAddress)
+    .delete('/:id', AddressController.deleteAddress)
 
 module.exports = addressRouter
