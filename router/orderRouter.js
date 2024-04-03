742183f07c9b0ddfb939706728b443dcd418be88
Muhammad Ridho <muhammadridho.mail@gmail.com>
2024-04-04 00:46:32 +0700

    add new route - getOrderById

--- router/orderRouter.js
+++ router/orderRouter.js
@@ -1,12 +1,14 @@
 const
     express = require('express'),
     orderRouter = express.Router(),
-    authenticate = require('../lib/auth.js'),
+    passport = require('../lib/passport.js'),
+    Auth = require('../lib/auth.js'),
     { OrderController } = require('../controller/orderController.js');
 
 
-orderRouter.use(authenticate)
+orderRouter.use(passport.initialize(), Auth.authentication, Auth.authorizeUser)
 orderRouter.get('/', OrderController.getAllOrders)
+orderRouter.get('/:id', OrderController.getOrderById)
 orderRouter.post('/', OrderController.createNewOrder)
 orderRouter.patch('/:id', OrderController.updateOrder)
 
