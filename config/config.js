0551faa4b6a6157dab4ec10c8bf9e3e8a21ea73f
Muhammad Ridho <muhammadridho.mail@gmail.com>
2024-04-04 00:42:39 +0700

    add timezone to development config

--- config/config.js
+++ config/config.js
@@ -7,20 +7,21 @@ const config = {
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
         dialect: process.env.DB_DIALECT,
+        timezone: '+07:00'
     },
     test: {
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
+        dialect: process.env.DB_DIALECT
     },
     production: {
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
+        dialect: process.env.DB_DIALECT
     }
 }
 
