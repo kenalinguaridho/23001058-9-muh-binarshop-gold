9dd121e9e056b17d4fd37742ea367d55a54aeeea
Muhammad Ridho <muhammadridho.mail@gmail.com>
2024-04-05 02:43:44 +0700

    add multer middleware to handle media upload

--- router/productRouter.js
+++ router/productRouter.js
@@ -3,6 +3,7 @@ const
     passport = require('../lib/passport'),
     express = require('express'),
     productRouter = express.Router(),
+    upload = require('../lib/multer.js'),
     { ProductController } = require('../controller/productController.js');
 
 productRouter
@@ -11,7 +12,7 @@ productRouter
 
 productRouter.use(passport.initialize(), Auth.authentication, Auth.authorizeAdmin)
 productRouter
-    .post('/', ProductController.createNewProduct)
+    .post('/',upload.array('products', 2), ProductController.createNewProduct)
     .put('/:id', ProductController.editProduct)
     .delete('/:id', ProductController.deleteProduct)
     .patch('/:id', ProductController.restock)
--- router/userRouter.js
+++ router/userRouter.js
@@ -3,19 +3,20 @@ const
     userRouter = express.Router(),
     passport = require('passport'),
     Auth = require('../lib/auth.js'),
+    upload = require('../lib/multer.js'),
     { UserController } = require('../controller/userController.js');
 
 userRouter
-    .post('/register', UserController.registerUser)
-    .post('/admin/register', UserController.registerAdmin)
+    .post('/register', upload.single('avatar'), UserController.registerUser)
+    .post('/admin/register', upload.single('avatar'), UserController.registerAdmin)
     .post('/login', UserController.login)
     .get('/verify/:id', UserController.verifyUser)
 
 userRouter.use(passport.initialize(), Auth.authentication)
 
 userRouter
-    .get('/profile' , UserController.getUser)
-    .put('/profile', UserController.editUser)
+    .get('/profile', UserController.getUser)
+    .put('/profile', upload.single('avatar'), UserController.editUser)
     .delete('/', UserController.deleteUser)
 
 module.exports = userRouter
