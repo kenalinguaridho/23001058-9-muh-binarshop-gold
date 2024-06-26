3f0904453c0a6bc8f403534cd111eae0e8f98891
Muhammad Ridho <muhammadridho.mail@gmail.com>
2024-06-26 15:40:41 +0700

    add base logic for data manipulation

---
+++ services/dataManipulationService.js
@@ -0,0 +1,32 @@
+const { CustomError } = require("../errors/customError")
+
+class DataManipulationService {
+    static create = async (model, payload, options) => {
+        try {
+            const newData = await model.create(payload, options)
+            return newData
+        } catch (error) {
+            throw error
+        }
+    }
+
+    static findById = async (model, identifier, options) => {
+        try {
+            const data = await model.findByPk(identifier, options)
+
+            if (!data) throw new CustomError('No record found')
+                
+            return data
+        } catch (error) {
+            throw error
+        }
+    }
+
+    // static getAll = async (model) => {
+
+    // }
+
+    // static getById = async ()
+}
+
+module.exports = { DataManipulationService }
