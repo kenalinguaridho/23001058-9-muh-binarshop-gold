5d27f58f2ec621588954071459d41642569e2c1a
Muhammad Ridho <muhammadridho.mail@gmail.com>
2024-06-26 15:36:45 +0700

    add custom error class to manipulate error response

---
+++ errors/customError.js
@@ -0,0 +1,27 @@
+class CustomError extends Error {
+    constructor(message, statusCode, details = {}) {
+        super(message)
+        this.statusCode = statusCode
+        this.details = details
+    }
+
+    static sequelizeError = (sequelizeError) => {
+        const errors = {}
+        let statusCode = 400
+
+        console.log("SEQUELIZE ERROR ==> ", sequelizeError);
+
+        for (let i = 0; i < sequelizeError.errors.length; i++) {
+            errors[sequelizeError.errors[i]["path"]] = sequelizeError.errors[i].message
+        }
+
+        if (sequelizeError.errors[0].type === 'unique violation') {
+            statusCode = 409
+        }
+
+        return new CustomError(sequelizeError.errors[0].type, statusCode, errors)
+    }
+
+}
+
+module.exports = { CustomError }
