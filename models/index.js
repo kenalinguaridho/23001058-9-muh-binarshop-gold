16e342f39691b8c69f9e4e6dd55a8c66b3103d71
Muhammad Ridho <muhammadridho.mail@gmail.com>
2025-06-15 17:09:08 +0700

    update variable used

--- models/index.js
+++ models/index.js
@@ -5,20 +5,16 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config')[env];
+const env = process.env.NODE_ENV;
+const config = require('../config/config.js')[env];
 const db = {};
 
 let sequelize;
 
-console.log("Config ==>", env);
-
-
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+sequelize = new Sequelize(config.database, config.username, config.password, {
+  host: config.host,
+  dialect: config.dialect
+});
 
 fs
   .readdirSync(__dirname)
